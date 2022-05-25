bd30b0be0ea7f0d7042113e58b3af0c22122ecfa
Juciane <jhucyy.pereira@gmail.com>
2022-05-24 23:30:26 -0300

    exercícios que tenho que fazer do curso pacotes

---
+++ atividades_a_fazer_curso_pacote.R
@@ -0,0 +1,9 @@
+
+# Atividades a fazer ------------------------------------------------------
+
+# https://curso-r.github.io/main-pacotes/slides/index.html#18
+
+# https://curso-r.github.io/main-pacotes/slides/index.html#18
+
+# https://curso-r.github.io/main-pacotes/slides/index.html#12
+
