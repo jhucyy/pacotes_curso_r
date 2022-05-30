70fb680ae40016d1d37994569bebf067c7b7e7e9
Juciane <56133164+jhucyy@users.noreply.github.com>
2022-05-30 20:57:41 -0300

    Add files via upload

---
+++ links_automatização.R
@@ -0,0 +1,23 @@
+https://github.com/beatrizmilz/noticiasgov 
+https://github.com/beatrizmilz/sigbm 
+
+https://github.com/beatrizmilz/sigbm/blob/main/.github/workflows/download-sigbm.yaml 
+
+https://github.com/r-lib/actions/tree/master/examples#readme 
+
+https://crontab.guru/#0_12_1_*_* 
+  
+  usethis::use_github_action("check-release")
+
+usethis::use_github_action("check-release") 
+usethis::use_github_action("test-coverage")
+
+
+https://beatrizmilz.com/talk/2021-github-actions-gyn/ 
+  
+  https://beatrizmilz.com/talk/2021-ser-uff/ 
+  
+
+  # devtools::document() 
+  
+  # usar isso para disponibilizar o pipe
